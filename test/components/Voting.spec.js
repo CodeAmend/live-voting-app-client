6e80ec4c7f2fbe75b88a10919352229817a8b4f5
Michael Bruce Allen <CodeAmend@Gmail.Com>
2017-07-02 14:26:00 -0500

    Enzyme added and test_helper removed

--- test/components/Voting.spec.js
+++ test/components/Voting.spec.js
@@ -1,21 +1,25 @@
 // Modules
 import React from 'react';
-import ReactTestUtils from 'react-dom/test-utils';
-
-// Load ReactTest Utils
-const {
-  renderIntoDocument
-} = ReactTestUtils; 
+import { expect } from 'chai';
+import { shallow } from 'enzyme';
 
 // Components
 import Voting from '../../src/components/Voting';
 
+
 describe('Voting', () => {
 
   it('renders a pair of buttons', () => {
-    const component = renderIntoDocument(
-      <Voting pair={["Trainspotting", "28 Days Later"]} />
+
+    const props = {
+      pair: ["Trainspotting", "28 Days Later"]
+    };
+
+    const component = shallow(<Voting {...props} />
     );
+
+    expect(component.find('button').length).to.equal(2);
+
   });
 
 });
