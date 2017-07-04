40a62faad5bd76fb6280404bae9d93eb64580bd7
Michael Bruce Allen <CodeAmend@Gmail.Com>
2017-07-03 22:27:24 -0500

    Results: compoent and route added

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,28 @@
 // Modules
 
 import React from 'react';
-import { Route } from 'react-router-dom';
-import { List } from 'immutable';
+import { Route, Switch } from 'react-router-dom';
+import { List, Map } from 'immutable';
 
 // Components
 import Voting from './Voting';
+import Results from './Results';
 
-const pair = List.of('Trainspotting', '28 Days')
+const pair = List.of('Trainspotting', '28 Days');
+const tally = Map({ 'Trainspotting': 4 , '28 Days': 4 });
 
 // Factory to add pair
 const PropRoute = (props) => {
-  return <props.component pair={pair} {...props}  />
+  return <props.component pair={pair} tally={tally}{...props}  />
 }
 
 class App extends React.Component {
   render() {
     return (
-      <PropRoute path="/" component={Voting}/>
+      <Switch>
+        <PropRoute exact path="/" component={Voting}/>
+        <PropRoute path="/results" component={Results} />
+      </Switch>
     );
   }
 }
---
+++ src/components/Results.js
@@ -0,0 +1,26 @@
+// Modules
+import React from 'react';
+
+export default (props) => {
+
+  const getPair = () => props.pair || [];
+
+  const getVotes = (entry) => {
+    if(props.tally && props.tally.has(entry))
+      return props.tally.get(entry);
+    return 0;
+  }
+
+  return (
+    <div className="voting">
+      {getPair().map( entry =>
+        <div key={entry} className="entry">
+          <h1>{entry}</h1>
+          <div className="vote-count">
+            {getVotes(entry)}
+          </div>
+        </div>
+      )}
+    </div>
+  );
+}
