372c5a81be31779f2fe2758e8d7897b6db831563
Michael Bruce Allen <CodeAmend@Gmail.Com>
2017-07-04 21:01:29 -0500

    Socket.io-client added and conntected to server

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import { HashRouter } from 'react-router-dom';
+import io from 'socket.io-client';
 
 // Components
 import App from './components/App';
@@ -11,8 +12,14 @@ import App from './components/App';
 // reducers
 import reducer from './reducer';
 
+console.log(location);
+const socket = io(`${location.protocol}//${location.hostname}:8090`)
 
 const store = createStore(reducer);
+
+socket.on('state', (state) => store.dispatch(
+  { type: 'SET_STATE', state }
+));
 
 store.dispatch({
   type: 'SET_STATE',
